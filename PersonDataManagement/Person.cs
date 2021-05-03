9a3685f5da3ea1b5f83fd649ebddff27711c8035
victor-singha <vicunite2@gmail.com>
2021-05-03 19:20:42 +0530

    [victor] person class created & added default values

---
+++ PersonDataManagement/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonDataManagement
+{
+    class Person
+    {
+        public string SSN;
+        public string Name;
+        public string Adress;
+        public int Age;
+
+        public Person(string ssn, string name, string adress, int age)
+        {
+            this.SSN = ssn;
+            this.Name = name;
+            this.Adress = adress;
+            this.Age = age;
+        }
+    }
+}
--- PersonDataManagement/Program.cs
+++ PersonDataManagement/Program.cs
@@ -10,6 +10,23 @@ namespace PersonDataManagement
     {
         static void Main(string[] args)
         {
+            List<Person> personList = new List<Person>() 
+            {
+                new Person("4578913", "Barack", "Washington", 65),
+                new Person("4561333","Donald","NewYork",62),
+                new Person("4567895","Sammy","Albama",17),
+                new Person("4598962", "Samuel", "NewJersey", 45),
+                new Person("203456879", "Smith", "12 Main Street, Newyork,NY", 45),
+                new Person("203456880", "SAM", "345 Main Ave, Dayton,OH", 55),
+                new Person("203456881", "Sue", "32 Cranbrook Rd, Newyork,NY", 65),
+                new Person("203456882", "Winston", "1208 Alex st, Newyork,NY", 65)
+            };
+
+            foreach(var person in personList)
+            {
+                Console.WriteLine(person.Adress);
+            }
+            Console.ReadKey();
         }
     }
 }
